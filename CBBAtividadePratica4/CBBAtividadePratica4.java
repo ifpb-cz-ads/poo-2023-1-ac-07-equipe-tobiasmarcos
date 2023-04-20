36236f2d76c8c3d8e76c9e33dd6d8282596225c2
Tobias-Santos <117104950+tobias-santos@users.noreply.github.com>
2023-04-20 19:22:06 -0300

    Adicionando questões do livro: Chagas, Baruque & Baruque, 1-6

---
+++ CBBAtividadePratica4/CBBAtividadePratica4.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica4
+{
+    public static void main(String []args){
+        int[] arr = new int [100];
+            
+            for(int k = 0; k <= 100; k ++){
+                arr[k] = k;
+                System.out.println(arr[k]);
+            }
+    }
+}
---
+++ CBBAtividadePratica5/CBBAtividadePratica5.java
@@ -0,0 +1,17 @@
+
+
+public class CBBAtividadePratica5
+{
+    public static void main(String []args){
+       String [] dias;
+       dias = new String[] {"Segunda", "Terça", "Quarta","Quinta", "Sexta",
+        "Sábado", "Domingo"};
+
+        int k=0;
+
+        while(dias.length > k){
+         System.out.println(dias[k]);
+         k ++;
+        }
+    }
+}
